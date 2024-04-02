5f643ad36a959fcf694d2b17a6a7ffe62940d829
Dev4Mod <wellingtonmods@gmail.com>
2024-04-02 16:52:23 -0300

    Revert "Update: Downgrade SDK to support android 9"
    This reverts commit d860065a00b8245704e5600b0ad39a7c004f68b8.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     buildFeatures { buildConfig = true }
     defaultConfig {
         applicationId = "its.madruga.wpp"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 34
         versionCode = 2
         versionName = "2.24.5.76"
